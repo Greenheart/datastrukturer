17ce1fbc50418110b6eb5d5c9e7f7da1488536ef
Samuel Plumppu <greenheartmusic@gmail.com>
2016-01-20 22:18:27 +0100

    Implemented UnorderedList
    Linting passing and most tests included though not with full
    coverage.

--- exercises/list.py
+++ exercises/list.py
@@ -2,6 +2,7 @@
 
 from .exceptions import EmptyList
 
+
 class Node():
     """Implementation av nod för `UnorderedList`.
     """
@@ -9,7 +10,8 @@ class Node():
     def __init__(self, data, next):
         """Initiera noden med attributen `self.data` och `self.next`.
         """
-        pass
+        self.data = data
+        self.next = next
 
 
 class UnorderedList():
@@ -21,51 +23,151 @@ class UnorderedList():
     def __init__(self):
         """Initiera den tomma listan.
         """
-        pass
+        self.head = None
 
     def is_empty(self):
         """Returnerar `True` om listan är tom, annars `False`.
         """
-        pass
+        return self.head is None
 
     def add(self, item):
         """Lägg till `item` i början av listan.
         """
-        pass
+        self.head = Node(item, self.head)
 
     def size(self):
         """Returnerar antalet värden i listan.
         """
-        pass
+        current = self.head
+        count = 0
+        while current:
+            count += 1
+            current = current.next
+        return count
 
     def search(self, item):
         """Returnerar `True` om `item` finns i listan, annars `False`.
         """
-        pass
+        current = self.head
+        while current:
+            if current.data is item:
+                return True
+            current = current.next
+
+        return False
 
     def remove(self, item):
         """Raderar första förekomsten av `item` från listan.
         """
-        pass
+        current = self.head
+        prev = None
+        while current:
+            if current.data == item:
+                if prev:    # If we have a node before the current item
+                    prev.next = current.next
+                else:
+                    self.head = current.next
+                return True
+
+            prev = current
+            current = current.next
+
+        return False    # Failure
 
     def append(self, item):
         """Lägg till `item` i slutet av listan.
         """
-        pass
+        current = self.head
+
+        if not current:  # List is empty
+            self.head = Node(item, None)
+            return True
+
+        while current:
+            if not current.next:
+                current.next = Node(item, None)
+                return True
+
+            current = current.next
+
+        return False
 
     def insert(self, position, item):
         """Lägg till `item` på index `position`.
         """
-        pass
+        current = self.head
+
+        if not current:  # No previous item, add to the beginning
+            self.head = Node(item, None)
+            return True
+
+        # Only init these vars if actually needed
+        index = 0
+        prev = None
+
+        while current:
+            if index == position:
+                if prev:
+                    prev.next = Node(item, current.next)
+                    return True
+                else:  # No previous item, add to the beginning
+                    self.head = Node(item, current.next)
+                    return True
+
+            index += 1
+            prev = current
+            current = current.next
+
+        return False
 
     def index(self, item):
         """Returnerar index i listan för första förekomsten av `item`.
         """
-        pass
+        if not self.is_empty():
+            index = 0
+            current = self.head
+            while current:
+                if current.data == item:
+                    return index
 
-    def pop(self, postition=None):
+                current = current.next
+                index += 1
+
+    def pop(self, position=None):
         """Plockar bort och returnerar värdet på index `position`.
 
         Om inget värde anges för `position` tolkas det som sista värdet.
         """
-        pass
+        current = self.head
+
+        if not current:  # List empty
+            return False
+        elif position and position > self.size() - 1:  # Invalid position
+            return False
+
+        # Only init these vars if actually needed
+        index = 0
+        prev = None
+
+        while current:
+            if position and index == position:
+                if prev:
+                    prev.next = current.next
+                    return True
+                else:  # No previous item, remove from beginning
+                    self.head = current.next
+                    return True
+            else:   # No pos given, just pop last item
+                if not current.next:
+                    if prev:
+                        prev.next = None
+                        return True
+                    else:
+                        self.head = None
+                        return True
+
+            index += 1
+            prev = current
+            current = current.next
+
+        return False
--- tests/test_list.py
+++ tests/test_list.py
@@ -1,9 +1,73 @@
 import unittest
 from exercises.list import Node, UnorderedList
 
+
 class NodeTests(unittest.TestCase):
-    pass
+
+    def test_node(self):
+        self.assertEqual(Node(5, None).data, 5)
 
 
 class UnorderedListTests(unittest.TestCase):
-    pass
+
+    def test_count(self):
+        ul = UnorderedList()
+        ul.add(5)
+        ul.add(8)
+        self.assertEqual(ul.size(), 2)
+
+    def test_is_empty(self):
+        ul = UnorderedList()
+        self.assertTrue(ul.is_empty())
+        ul.add(4)
+        self.assertFalse(ul.is_empty())
+
+    def test_search(self):
+        ul = UnorderedList()
+        ul.add(5)
+        ul.add(4)
+        ul.add(6)
+        ul.add(7)
+        ul.add(8)
+        self.assertTrue(ul.search(7))
+
+    def test_remove(self):
+        ul = UnorderedList()
+        ul.add(5)
+        ul.add(6)
+        self.assertFalse(ul.remove(4))
+        self.assertTrue(ul.remove(5))
+        self.assertEqual(ul.size(), 1)
+        self.assertTrue(ul.remove(6))
+
+    def test_index(self):
+        ul = UnorderedList()
+        ul.add(5)
+        ul.add(4)
+        ul.add(5)
+        ul.add(6)
+        self.assertEqual(ul.index(8), None)
+        self.assertEqual(ul.index(5), 1)
+
+    def test_insert(self):
+        ul = UnorderedList()
+        ul.add(5)
+        ul.add(3)
+        self.assertTrue(ul.insert(0, 5))
+        self.assertTrue(ul.search(5))
+
+    def test_pop(self):
+        ul = UnorderedList()
+        self.assertFalse(ul.pop(2))
+        ul.add(5)
+        self.assertTrue(ul.pop(0))
+        ul.add(5)
+        self.assertFalse(ul.pop(2))
+        self.assertTrue(ul.pop())
+        self.assertEqual(ul.size(), 0)
+
+    def test_append(self):
+        ul = UnorderedList()
+        self.assertTrue(ul.append(5))
+        self.assertEqual(ul.size(), 1)
+        self.assertTrue(ul.append("a"))
